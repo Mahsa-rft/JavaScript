1f6bc439a9f985037d426f883f5568ea8ff6a78b
mahsa rafat <mahsa_rft@icloud.com>
2017-08-06 13:11:57 +0430

    if&else if

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,19 @@
  * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
-
+/*
 alert("Hello World");
 confirm("i love JS");
 prompt("do you want to learn js?");
+*/
+
+var favoriteCar = prompt("what is your favorite car?");
+if(favoriteCar==="santafe"){
+    alert("santafe is great!");
+}
+else if (favoriteCar=== "i30"){
+    alert("i30 is beautiful");
+}
+else{
+    alert("Good Choice");
+}
